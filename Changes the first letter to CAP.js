e3fa0c9f4f0b23a1c502c7ba0c35e59f61fbc948
Argenis Flores <argenisflowers@gmail.com>
2022-09-09 09:07:25 -0400

    First Letter to CAP 1.1

---
+++ Changes the first letter to CAP.js
@@ -0,0 +1,10 @@
+//prompt user for name
+var name = prompt(What is your name);
+//extract letter to cap
+var charCap = name.slice(0,0);
+// 
+var charRem = name.slice(1);
+
+console.log(name);
+console.log(charCap);
+console.log(charRem); 
