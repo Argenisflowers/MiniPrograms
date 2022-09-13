4ed0af2c3d1da31fd6590fd31274c43506f947a6
Argenis Flores <argenisflowers@gmail.com>
2022-09-13 09:56:20 -0400

    Program to capitalise first letter of a name complete

--- Changes the first letter to CAP.js
+++ Changes the first letter to CAP.js
@@ -1,10 +1,13 @@
-//prompt user for name
-var name = prompt(What is your name);
-//extract letter to cap
-var charCap = name.slice(0,0);
-// 
-var charRem = name.slice(1);
+var name = prompt("What is your name");
+
+var charCap = name.slice(0,1);
+
+var charRem = name.slice(1,name.lenght);
+
+charCap = charCap.toUpperCase();
+
+charRem = charRem.toLowerCase();
 
 console.log(name);
-console.log(charCap);
-console.log(charRem); 
+
+console.log(charCap + charRem);
