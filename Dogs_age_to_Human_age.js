132c808cf38db369c2329c59b94fa87d4d37ed5b
Argenis Flores <argenisflowers@gmail.com>
2022-09-13 10:50:30 -0400

    dogs_to_human_age_converter complete

--- Dogs_age_to_Human_age.js
+++ Dogs_age_to_Human_age.js
@@ -1,1 +1,10 @@
 //Dog age to human age formula is humanAge =(dogAge - 2) * 4 + 21;
+
+var getDogsAge = prompt("What is the Dogs age?");
+
+var humanAge = ((getDogsAge - 2) * 4) + 21;
+
+console.log(humanAge);
+console.log(getDogsAge);
+alert('Your dog is '+ humanAge +' years old in human years', humanAge);
+console.log("Your dog is %i years old in human years", humanAge);
