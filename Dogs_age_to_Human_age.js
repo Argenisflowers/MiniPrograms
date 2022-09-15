134fc39b0a3cf669d3ece66d7a565bc849a13dba
Argenis Flores <argenisflowers@gmail.com>
2022-09-15 11:23:05 -0400

    Love calculator complete

--- Dogs_age_to_Human_age.js
+++ Dogs_age_to_Human_age.js
@@ -5,6 +5,7 @@ var getDogsAge = prompt("What is the Dogs age?");
 var humanAge = ((getDogsAge - 2) * 4) + 21;
 
 console.log(humanAge);
+
 console.log(getDogsAge);
+
 alert('Your dog is '+ humanAge +' years old in human years', humanAge);
-console.log("Your dog is %i years old in human years", humanAge);
---
+++ Love calculator.js
@@ -0,0 +1,9 @@
+yourName = prompt("Your name: ");
+
+nameOfCrush = prompt("Name of your crush: ")
+
+var number = Math.random();
+number = (number * 100) + 1;
+number = Math.floor(number);
+
+alert("You are "+ number +"% compatible");
